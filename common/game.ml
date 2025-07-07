87e054f369ee5c9ec6f9b853c9606d38ad9088b9
StephenTrainor <stephontrainor@gmail.com>
2025-07-07 21:00:46 +0000

    Added helper function to retrieve all valid positions for a given game board

--- common/game.ml
+++ common/game.ml
@@ -8,3 +8,10 @@ let empty game_kind = { game_kind; board = Position.Map.empty }
 
 let set_piece t position piece =
   { t with board = Map.set t.board ~key:position ~data:piece }
+
+let get_all_valid_positions t : Position.t list =
+  let board_length = Game_kind.board_length t.game_kind in
+  List.init (board_length * board_length) ~f:(fun i ->
+      let column = i % board_length in
+      let row = i / board_length in
+      ({ row; column } : Position.t))
--- common/game.mli
+++ common/game.mli
@@ -5,3 +5,4 @@ type t = { game_kind : Game_kind.t; board : Piece.t Position.Map.t }
 
 val empty : Game_kind.t -> t
 val set_piece : t -> Position.t -> Piece.t -> t
+val get_all_valid_positions : t -> Position.t list
